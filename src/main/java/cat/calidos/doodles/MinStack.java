0036dd39305b6984131db1318b340472613d33d0
Daniel Giribet <dani@calidos.cat>
2015-03-10 11:41:17 +0100

    Found a bug reviewing the code

--- src/main/java/cat/calidos/doodles/MinStack.java
+++ src/main/java/cat/calidos/doodles/MinStack.java
@@ -18,6 +18,7 @@ package cat.calidos.doodles;
 // How would you design a stack which, in addition to push and pop, also has a 
 // function min which returns the minimum element? Push, pop and min should 
 // all operate in O(1) time 
+// FIXME: if we push two minimums that are equal we have an issue!
 public class MinStack<T> extends Stack<T> {
 
 protected Stack<T> min;
