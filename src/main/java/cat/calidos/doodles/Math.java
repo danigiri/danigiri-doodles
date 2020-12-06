557fab7304bb683359d146881e5462ccdb218db9
Daniel Giribet <dani@calidos.cat>
2020-12-06 23:22:41 +0100

    steps problem

--- src/main/java/cat/calidos/doodles/Math.java
+++ src/main/java/cat/calidos/doodles/Math.java
@@ -16,6 +16,8 @@
 
 package cat.calidos.doodles;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.IntStream;
 
 /**
@@ -39,7 +41,64 @@ public static int divideWithSubstract(double a, double b) {
 		a -= b;
 		d++;
 	}
+
 	return d;
+
 }
+
+
+//8.1 triple step
+//a child is running up a staircase with n steps, can hop 1, 2 or 3 steps
+//how many possible ways can the child go up the stairs?
+
+//step(n) = 	n==0 → 0
+//		n==1 → 1
+//		n==2 → 2
+//		n==3 → [1,1,1],[1,2],[2,1],[3]:4 
+//		n>3  → 1+step(n-1)+1+step(n-2)+1+step(n-3)
+
+//we can do this recursively but cache the results so we do not recalculate everything
+
+public static int steps(int n) {
+	return _steps(n, new HashMap<Integer, Integer>(n));
+}
+
+
+private static int _steps(int n, Map<Integer, Integer> cache) {
+
+	int steps;
+
+	if (n==0) {
+		steps = 0;
+	} else if (n==1) {
+		steps = 1;
+	} else if (n==2) {
+		steps = 2;
+	} else if (n==3) {
+		steps = 4;
+	} else {
+		steps = 3+_getStepsCached(n-1, cache)+_getStepsCached(n-2, cache)+_getStepsCached(n-3, cache);
+	}
+
+	return steps;
+
+}
+
+
+private static int _getStepsCached(int n, Map<Integer, Integer> cache) {
+
+	int steps;
+
+	if (cache.containsKey(n)) {
+		steps = cache.get(n);		// O(k)
+	} else {
+		steps = _steps(n, cache);	// O(n)
+		cache.put(n, steps);		// O(k)
+	}
+
+	return steps;
+
+}
+
 
 }
--- src/test/java/cat/calidos/doodles/MathTest.java
+++ src/test/java/cat/calidos/doodles/MathTest.java
@@ -1,28 +1,15 @@
-/**
- Copyright 2016 Daniel Giribet <dani - calidos.cat>
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
+// MATH TEST . JAVA
 package cat.calidos.doodles;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 
 
 public class MathTest {
 
-@Test
+@Test @DisplayName("multiply with additions test")
 public void multiplyWithPlusTest() {
 
 	int v = Math.multiplyWithPlus(0, 0);
@@ -51,7 +38,8 @@ public void multiplyWithPlusTest() {
 
 }
 
-@Test
+
+@Test @DisplayName("divide with substractions test")
 public void divideWithSubstractTest() {
 
 	int v = Math.divideWithSubstract(0.0, 1.0);
@@ -59,10 +47,41 @@ public void divideWithSubstractTest() {
 
 	v = Math.divideWithSubstract(1.0, 0.5);
 	assertEquals(2, v);
-	
+
 	v = Math.divideWithSubstract(0.5, 1.0);
 	assertEquals(0, v);
 
 }
 
+
+@Test  @DisplayName("multiply with additions test")
+public void stepTest() {
+
+	assertEquals(0, Math.steps(0));
+	assertEquals(1, Math.steps(1));
+	assertEquals(2, Math.steps(2));
+	assertEquals(4, Math.steps(3));
+
+	// 4= 1+steps(3):4+1+steps(2):2+1+steps(1):1 = 10
+	assertEquals(10, Math.steps(4));
+
 }
+
+
+}
+
+/**
+Copyright 2016 Daniel Giribet <dani - calidos.cat>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
