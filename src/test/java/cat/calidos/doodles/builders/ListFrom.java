573a7107887bfeb4020dd9993adfb7a69067cc08
Daniel Giribet <dani@calidos.cat>
2021-02-21 23:16:52 +0100

    minor changes

--- src/test/java/cat/calidos/doodles/builders/ListFrom.java
+++ src/test/java/cat/calidos/doodles/builders/ListFrom.java
@@ -1,28 +1,14 @@
-/**
- Copyright 2014 Daniel Giribet <dani - calidos.cat>
+// LIST FROM . JAVA
 
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
 package cat.calidos.doodles.builders;
 
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class ListFrom {
 
 public static List<Integer> ints(int... v) {
-	
+
 	List<Integer> a = new ArrayList<Integer>(v.length);
 	for (int i : v) {
 		a.add(Integer.valueOf(i));
@@ -52,3 +38,19 @@ public static <T> List<T> list(List<T> l) {
 
 
 }
+
+/**
+ Copyright 2014 Daniel Giribet <dani - calidos.cat>
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
--- src/test/java/cat/calidos/doodles/builders/QueueFrom.java
+++ src/test/java/cat/calidos/doodles/builders/QueueFrom.java
@@ -1,45 +1,50 @@
-/**
- Copyright 2014 Daniel Giribet <dani - calidos.cat>
+// QUEUE FROM . JAVA
 
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-*/
 package cat.calidos.doodles.builders;
 
 import cat.calidos.doodles.Queue;
 
-
-
 public class QueueFrom {
 
+
 public static Queue<String> strings(String... v) {
-	
+
 	Queue<String> a = new Queue<String>();
 	for (String i : v) {
 		a.enqueue(i);
 	}
+
 	return a;
-	
+
 }
 
 
 public static Queue<Integer> ints(Integer... v) {
-	
+
 	Queue<Integer> a = new Queue<Integer>();
 	for (Integer i : v) {
 		a.enqueue(i);
 	}
+
 	return a;
-	
-}
 
 }
+
+
+}
+
+/**
+Copyright 2014 Daniel Giribet <dani - calidos.cat>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
