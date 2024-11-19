2446003529b772ff8309174d195d8442292082ab
Daniel Giribet <dani@calidos.cat>
2024-11-20 00:59:20 +0100

    import seems to work  though chai gives a warning

--- src/main/typescript/tests/hello.test.ts
+++ src/main/typescript/tests/hello.test.ts
@@ -1,25 +1,5 @@
-// import { expect, test } from 'vitest';
-//const vt = await import('vitest');
-// const vt = require('vitest');
+import { assert } from 'chai';
 
-// import { assert } from 'chai';
-//const assert = require('chai').assert;
-//const chai = await import('chai');
-//import chai = require('chai');
-//import * as chai from 'chai';
-//const assert = require('chai').assert;
-
-//chai.assert.equal('bar', foo);
-//assert.equal('bar', foo);
-/*
-vt.test('adds 1 + 2 to equal 3', () => {
-let foo:string = 'bar'
-  vt.expect(foo).toBe('bar')
-})
-
-*/
-
-const assert = require('chai').assert
 const foo:string = 'foo';
 
 assert.equal(foo, 'foo');
